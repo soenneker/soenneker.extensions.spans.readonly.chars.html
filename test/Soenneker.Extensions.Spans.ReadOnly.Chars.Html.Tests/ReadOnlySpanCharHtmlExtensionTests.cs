5f91695abc78aa5b43dd7803e2711621e2c09e75
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:04 -0500

    TUnit conversion

--- test/Soenneker.Extensions.Spans.ReadOnly.Chars.Html.Tests/ReadOnlySpanCharHtmlExtensionTests.cs
+++ test/Soenneker.Extensions.Spans.ReadOnly.Chars.Html.Tests/ReadOnlySpanCharHtmlExtensionTests.cs
@@ -1,11 +1,10 @@
 using Soenneker.Tests.Unit;
-using Xunit;
 
 namespace Soenneker.Extensions.Spans.ReadOnly.Chars.Html.Tests;
 
 public sealed class ReadOnlySpanCharHtmlExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
